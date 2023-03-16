f92a731b8d96da0bad0b5a079a488d2c7077ccdb
sid <shtrived@gmail.com>
2023-03-16 00:26:43 -0700

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center hero">
+    <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
+    <h1 class="mb-4">Speciality Pizza for your next Corporate Event</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HeroContent"
+};
+</script>
